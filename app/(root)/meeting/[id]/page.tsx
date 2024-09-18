6c7ff0e5d6ef5cab24859849ed1797d388e5d7e7
amitdutta <duttaamit356@gmail.com>
2024-09-18 12:04:38 +0530

    Implemented meeting setup and call

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,21 +1,41 @@
 'use client'
 
+import Loader from '@/components/Loader';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetUp from '@/components/MeetingSetUp';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
-import React from 'react'
+import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+
+    const { isLoaded, user } = useUser();
+    const { call, isCallLoading } = useGetCallById(id);
+    const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+    if (!isLoaded || isCallLoading) return <Loader />;
+
+    if (!call) return (
+        <p className="text-center text-3xl font-bold text-white">
+            Call Not Found
+        </p>
+    );
+
     return (
         <main className="h-screen w-full">
             <StreamCall
-            //   call={call}
+                call={call}
             >
                 <StreamTheme>
-                    {/* 
-        {!isSetupComplete ? (
-          <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-        ) : (
-          <MeetingRoom />
-        )} */}
+
+                    {!isSetupComplete ? (
+                        <MeetingSetUp
+                            setIsSetupComplete={setIsSetupComplete}
+                        />
+                    ) : (
+                        <MeetingRoom />
+                    )}
                 </StreamTheme>
             </StreamCall>
         </main>
---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,17 @@
+
+const MeetingRoom = () => {
+
+
+
+    return (
+        <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+            <div className="relative flex size-full items-center justify-center">
+                <div className=" flex size-full max-w-[1000px] items-center">
+                    {/* <CallLayout /> */}Room
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MeetingRoom
